e3b564f8d497f55c12a766ff2310fc89a39556c5
AbiSHek-071 <159239758+AbiSHek-071@users.noreply.github.com>
2024-08-01 14:30:59 +0530

    Create object manupulation.js

---
+++ object manupulation.js
@@ -0,0 +1,13 @@
+//adding a new key value pair into an object
+const obj = {name:"abhishek p "};
+obj["age"] = 21;
+
+//changing a value of existing key
+obj["name"] = "lekshmi shaly"
+console.log(obj)
+
+//deleting 
+const obj2 = {name:"Thomas Shelby",age:71,phone:8372912923};
+delete obj2.phone
+console.log(obj2)
+
