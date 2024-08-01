e61eaab9a2f861ea4da9357e2f687c3b9bad1998
AbiSHek-071 <159239758+AbiSHek-071@users.noreply.github.com>
2024-08-01 14:15:52 +0530

    Create array of objects representing people with name and age properties, create a new array containing only the names of these people using map().js

---
+++ array of objects representing people with name and age properties, create a new array containing only the names of these people using map().js
@@ -0,0 +1,9 @@
+const arr = [
+    {name:"Abhishek",age:20},
+    {name:"Lekshmi shaly",age:21},
+    {name:"Tomas Shelby",age:52},
+    ];
+const newarr = arr.map((x)=>{
+    return x.name;
+})
+    console.log(newarr)
